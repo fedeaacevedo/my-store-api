f5573165152b458ec253216e91a773f59f2f10e2
Federico <federicoaacevedo@hotmail.com>
2023-05-19 01:09:11 -0300

    commit inicial

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Servidor node");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get('/',(req, res) => {
+  res.send("Hola mundo")
+})
+
+app.listen(port, () => {
+  console.log('Estas usando el puerto: '+ port);
+});
+
