83b7cd2aeca74c8eebdb35ca1e8a18c69f88b86a
Federico <federicoaacevedo@hotmail.com>
2023-05-19 01:13:04 -0300

    routes

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get('/',(req, res) => {
   res.send("Hola mundo")
 })
 
+
+app.get('/nueva-ruta',(req, res) => {
+  res.send("Hola, soy un nuevo endpoint")
+})
+
+
+app.get('/productos',(req, res) => {
+  res.send("Hola, soy la pagina de productos")
+})
+
 app.listen(port, () => {
   console.log('Estas usando el puerto: '+ port);
 });
