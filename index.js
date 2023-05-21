2c5767b95cc233ec32c9b4f2b4c6f76033d0c566
Federico <federicoaacevedo@hotmail.com>
2023-05-21 00:29:29 -0300

    faker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {faker} = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
@@ -13,8 +14,49 @@ app.get('/nueva-ruta',(req, res) => {
 })
 
 
-app.get('/productos',(req, res) => {
-  res.send("Hola, soy la pagina de productos")
+app.get('/products',(req, res) => {
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.imageUrl(),
+    });
+
+  }
+  res.json(products);
+});
+
+app.get('/products/:id',(req, res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+    name:'Producto 2',
+    price: 5000
+  })
+})
+
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId
+  })
+})
+
+app.get('/users', (req,res) => {
+  const {limit, offset} = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }else{
+    res.send('no hay parametros')
+  }
 })
 
 app.listen(port, () => {
